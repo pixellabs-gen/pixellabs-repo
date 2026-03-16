3c0c1f38c27e802081748fb709fe41643893f40a
pixellabs <pixellabsdev@github.com>
2026-03-16 05:09:13 -0500

    build(lint): add eslint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  ignorePatterns: ['dist', 'node_modules', 'coverage'],
+  overrides: [
+    {
+      files: ['**/*.test.ts'],
+      env: {
+        node: true,
+      },
+    },
+  ],
+};
